240ecff15f501152781b7ad3bf50bd2a08b9ef65
Travis Garcia <tgar@ucdavis.edu>
2021-07-13 17:27:32 -0700

    setup frontend skeleton

---
+++ Frontend/src/App.js
@@ -0,0 +1,71 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+function App() {
+  const [post, setpost] = useState();
+
+  const handleChange = (event) => {
+    setpost(event.target.value);
+  };
+
+  const handlePostSubmit = () => {
+    const date = new Date();
+    const requestBody = {
+      post: post,
+      date: date,
+    };
+    const requestOptions = {
+      method: "POST",
+      requestBody: requestBody,
+    };
+    const postPromise = new Promise((resolve, reject) => {
+      fetch("", requestOptions)
+        .then((response) => {
+          if (response.ok) {
+            // gets run if request succeeds
+            resolve("Success");
+          } else {
+            // gets run if request fails
+            return Promise.reject(response.status);
+          }
+        })
+        .catch((err) => {
+          console.log("error uploading post: ", err);
+          reject(err);
+        });
+    }, 5000);
+
+    postPromise
+      .then((message) => {
+        // from resolve
+        console.log("promise resolved");
+      })
+      .catch((message) => {
+        // message from reject
+        console.log("promise rejected, err: ", message);
+      });
+  };
+
+  return (
+    <div className="post">
+      <TextField
+        id="outlined-multiline-static"
+        label="Multiline"
+        multiline
+        rows={4}
+        defaultValue="Default Value"
+        variant="outlined"
+        onChange={handleChange}
+      />
+      <Button variant="outlined" color="primary" onClick={handlePostSubmit}>
+        Submit
+      </Button>
+    </div>
+  );
+}
+
+export default App;
